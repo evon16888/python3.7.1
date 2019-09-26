791483de8e2a727718d1a3ae6492788294c90d57
evon16888 <evon16888@gmail.com>
2019-09-26 09:14:19 +0800

    Update bin.sh

--- bin.sh
+++ bin.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
 yum -y install zlib\*
 yum install libffi-devel -y
 yum -y groupinstall "Development tools"
